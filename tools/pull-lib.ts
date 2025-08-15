8e36cfb4746586e55a49e8a7c11324299116b1e2
ericchase <github@eric.addymail.com>
2025-08-15 13:24:50 -0500

    up
    Signed-off-by: ericchase <github@eric.addymail.com>

--- tools/pull-lib.ts
+++ tools/pull-lib.ts
@@ -7,11 +7,12 @@ import { Step_FS_Mirror_Directory } from './core/step/Step_FS_Mirror_Directory.j
 // This script pulls template lib files from template project.
 
 const template_path = 'C:/Code/Base/JavaScript-TypeScript/Templates/Discord-Bot';
+const lib_folders: string[] = ['lib-discord-bot'];
 
 Builder.SetStartUpSteps(
   Step_Bun_Run({ cmd: ['bun', 'install'], showlogs: false }),
   Step_Async(
-    ['lib-discord-bot'].map((dir: string) =>
+    lib_folders.map((dir: string) =>
       Step_FS_Mirror_Directory({
         from_path: NODE_PATH.join(template_path, 'tools/' + dir),
         to_path: NODE_PATH.join(Builder.Dir.Tools, dir),
