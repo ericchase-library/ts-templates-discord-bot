f73123f0afe28f3f34f8fbe865896bd43022b9a5
ericchase <github@eric.addymail.com>
2025-08-17 11:59:12 -0500

    up
    Signed-off-by: ericchase <github@eric.addymail.com>

--- tools/build.ts
+++ tools/build.ts
@@ -7,10 +7,11 @@ import { Step_Bun_Run } from './core/step/Step_Bun_Run.js';
 import { Step_FS_Clean_Directory } from './core/step/Step_FS_Clean_Directory.js';
 import { Step_Run_Discord_Bot } from './lib-discord-bot/steps/Step_Run_Discord_Bot.js';
 
-// Use command line arguments to set dev mode.
+// Use command line arguments to set developer mode.
 if (BunPlatform_Args_Has('--dev')) {
   Builder.SetMode(Builder.MODE.DEV);
 }
+// Set the logging verbosity
 Builder.SetVerbosity(Builder.VERBOSITY._1_LOG);
 
 // These steps are run during the startup phase only.
@@ -26,21 +27,21 @@ Builder.SetStartUpSteps(
 // These steps are run before each processing phase.
 Builder.SetBeforeProcessingSteps();
 
-// Basic setup for a TypeScript powered project. TypeScript files that match
-// "*.module.ts" and "*.iife.ts" are bundled and written to the out folder.
-// The other TypeScript files do not produce bundles. Module ("*.module.ts")
-// files will not bundle other module files. Instead, they'll import whatever
-// exports are needed from other module files. IIFE ("*.iife.ts") files, on
-// the other hand, produce fully contained bundles. They do not import anything
-// from anywhere. Use them accordingly.
+// Basic setup for a TypeScript project. TypeScript files that match
+// "*.module.ts" and "*.iife.ts" are bundled and written to the out folder. The
+// other TypeScript files do not produce bundles. Module scripts
+// ("*.module.ts") will not bundle other module scripts. Instead, they'll
+// import whatever exports are needed from other module scripts. IIFE scripts
+// ("*.iife.ts"), on the other hand, produce fully contained bundles. They do
+// not import anything from anywhere. Use them accordingly.
 
 // HTML custom components are a lightweight alternative to web components made
-// possible by the processors below.
+// possible by the processor I wrote.
 
 // The processors are run for every file that added them during every
 // processing phase.
 Builder.SetProcessorModules(
-  // Bundle the iife scripts and modules.
+  // Bundle the IIFE scripts and module scripts.
   Processor_TypeScript_Generic_Bundler({ target: 'bun' }, { bundler_mode: 'iife' }),
   Processor_TypeScript_Generic_Bundler({ target: 'bun' }, { bundler_mode: 'module' }),
   //
@@ -48,14 +49,14 @@ Builder.SetProcessorModules(
 
 // These steps are run after each processing phase.
 Builder.SetAfterProcessingSteps(
-  // During "dev" mode (when "--dev" is passed as an argument), the bot
-  // client will start running with automatic re-running when output files
-  // change. Look in the "Dev-StartClient.ts" file to see how it works.
+  // During developer mode (see above), the bot client will start running with
+  // automatic re-running when source files are modified. Look in the
+  // "Dev-StartClient.ts" file to see how it works.
   Step_Run_Discord_Bot(),
   //
 );
 
-// These steps are run during the shutdown phase only.
+// These steps are run during the cleanup phase only.
 Builder.SetCleanUpSteps();
 
 await Builder.Start();
